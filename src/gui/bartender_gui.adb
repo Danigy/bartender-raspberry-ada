3db012af3b9494d7baa8c5497db393f251ee4095
Yann Bonnet <yann.bonnet@epita.fr>
2019-01-22 22:31:22 +0100

    Add basic gui interface, no action when clicking on buttons
    Signed-off-by: Yann Bonnet <yann.bonnet@epita.fr>

---
+++ src/gui/bartender_gui.adb
@@ -0,0 +1,51 @@
+package body Bartender_GUI is
+
+	function BartenderwinBasic return BarWinAccess is
+		ret : BarWinAccess;
+		bartenderObj : BartenderWindow;
+		addBottle : Gtk_Menu_Item;
+		addRecipe : Gtk_Menu_Item;
+		butts : ButtonArray(1..100);
+		title : String := "Ada Bartender";
+		
+	begin
+		-- setting up bartenderObj.Windowdow object
+		Gtk_New(bartenderObj.Window);
+		bartenderObj.Window.Set_Title(title);
+		bartenderObj.Window.set_default_size(400,400);
+		
+		-- setting up main box object
+		Gtk_New_VBox(bartenderObj.MainBox);
+
+		-- setting up bartenderObj.MenuBar bar
+		Gtk_New(bartenderObj.MenuBar);
+		bartenderObj.MenuBar.set_pack_direction(Pack_Direction_LTR);
+		bartenderObj.MainBox.pack_start(bartenderObj.MenuBar, Expand => false, Fill => true);
+		-- setting up bartenderObj.MenuBar items
+		Gtk_New(addBottle, "Add Bottle");
+		Gtk_New(addRecipe, "Add Recipe");
+		bartenderObj.MenuBar.Append(addRecipe);
+		bartenderObj.MenuBar.Append(addBottle);		
+
+
+		-- setting up recipes box / buttons
+		Gtk_New_VBox(bartenderObj.RecipeBox);
+		for i in 1..100 loop
+			Gtk_New(butts(i), "recipe ");
+			bartenderObj.RecipeBox.Pack_End(butts(i));
+		end loop;
+
+		-- setting up Scroll
+		Gtk_New(bartenderObj.Scroll);
+		bartenderObj.Scroll.Set_Policy(Policy_Automatic, Policy_Automatic);
+		bartenderObj.Scroll.add_with_viewport(bartenderObj.RecipeBox);
+		bartenderObj.MainBox.pack_start(bartenderObj.Scroll, Expand => true, Fill => true);
+
+		bartenderObj.Window.Add(bartenderObj.MainBox);
+		bartenderObj.RecipesButts := new ButtonArray'(butts);
+
+		ret := new BartenderWindow'(bartenderObj);
+		return ret;
+	end BartenderWinBasic;
+
+end Bartender_GUI;
