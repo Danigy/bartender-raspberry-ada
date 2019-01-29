b92481063396072a11406b69f29c4a40c740fd7e
Corentin LE BIGOT <lebigo_c@epita.fr>
2019-01-29 19:35:27 +0100

    pump_csv: Fix pump_csv to comply with new GPIO Type
    Signed-off-by: Corentin LE BIGOT <lebigo_c@epita.fr>

--- src/pump_csv/pump_csv.adb
+++ src/pump_csv/pump_csv.adb
@@ -1,5 +1,5 @@
 with Ada.Text_IO; use Ada.Text_IO;
-with Draughts, Bottles;
+with Draughts, Bottles, GPIO;
 
 package body Pump_CSV is
 
@@ -23,21 +23,21 @@ package body Pump_CSV is
     function ParseDraughtCSV (Content : String) return Draughts.Draught is
         First : Integer := Content'First;
         Next : Integer := CSVNextElement(Content, First);
-        GPIO : Integer := Integer'Value(Content(First .. Next - 1));
-        Flow : Float := 0.0;
+        IO : Integer := GPIO.libsimpleio.Create(0, Integer'Value(Content(First .. Next - 1)), GPIO.Output);
+        Flow : Integer := 0;
         Bottle_Name : Bottles.String_Access := null;
         Remaining_Vol : Integer := 0;
     begin
         First := Next + 1;
         Next := CSVNextElement(Content, First);
-        Flow := Float'Value(Content(First .. Next - 1));
+        Flow := Integer'Value(Content(First .. Next - 1));
         First := Next + 1;
         Next := CSVNextElement(Content, First);
         Bottle_Name := new String'(Content(First .. Next - 1));
         First := Next + 1;
         Next := CSVNextElement(Content, First);
         Remaining_Vol := Integer'Value(Content(First .. Next - 1));
-        return ((Bottle_Name,  Remaining_Vol), (GPIO, Flow));
+        return ((Bottle_Name,  Remaining_Vol), (IO, Flow));
     end ParseDraughtCSV;
 
     function CSVNextElement (Line : String; First : Integer) return Integer is
