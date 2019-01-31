2722cb0bd8576c0860737c161dca7c79c1901948
Corentin LE BIGOT <lebigo_c@epita.fr>
2019-01-31 20:43:07 +0100

    bottle: Reenforce contracts
    Reenforce contract on RemoveRemainingVolume.
    
    Signed-off-by: Corentin LE BIGOT <lebigo_c@epita.fr>

--- src/bottle/bottles.ads
+++ src/bottle/bottles.ads
@@ -1,16 +1,14 @@
 with Strings; use Strings;
 
 package Bottles is
-    subtype Volume is Integer range 0 .. Integer'Last;
 
     type Bottle is tagged record
         Name : String_Access;
-        Remaining_Vol : Volume; -- Remaining Volume in the bottle in ml
+        Remaining_Vol : Positive; -- Remaining Volume in the bottle in ml
     end record;
-       with Dynamic_Predicate => This.Remaining_Vol > 0;
 
-    procedure RemoveRemainingVolume (bot: in out Bottle; Vol : Positive);
+    procedure RemoveRemainingVolume (bot: in out Bottle; Vol : Positive)
         with Pre => bot.Remaining_Vol > Vol,
-             Post => bot.Remaining_vol >= 0 and then bot'old.Remaining_Vol - Vol > 0;
+             Post => bot.Remaining_vol >= 0 and then bot'old.Remaining_Vol - Vol = bot.Remaining_Vol and then bot'Old.Name = bot.Name;
 
 end Bottles;
