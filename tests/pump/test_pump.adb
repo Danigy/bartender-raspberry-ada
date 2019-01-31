45c8b70b9247f080523bb472d3657f43fe3316f1
Corentin LE BIGOT <lebigo_c@epita.fr>
2019-01-31 20:25:02 +0100

    pump: Change Flow and NB types.
    Used subtype to add constraint.
    
    Signed-off-by: Corentin LE BIGOT <lebigo_c@epita.fr>

--- tests/pump/test_pump.adb
+++ tests/pump/test_pump.adb
@@ -1,7 +1,7 @@
-with Pumps;
+with Pumps, GPIO, GPIO.libsimpleio;
 
 procedure Test_Pump is
-    Pu : Pumps.Pump := (GPIO.libsimpleio.Create(0, 15, GPIO.Output), 10);
+    Pu : Pumps.Pump := (GPIO.libsimpleio.Create(0, 15, GPIO.Output), 10, 15);
 begin
     null;
 end Test_Pump;
