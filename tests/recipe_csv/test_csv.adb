e2e2154ed193a5ce104491688ce268f8bf02fe7d
Corentin LE BIGOT <lebigo_c@epita.fr>
2019-01-31 23:13:44 +0100

    tests: Add recipe csv tests
    Signed-off-by: Corentin LE BIGOT <lebigo_c@epita.fr>

--- tests/recipe_csv/test_csv.adb
+++ tests/recipe_csv/test_csv.adb
@@ -6,6 +6,8 @@ procedure Test_CSV is
     List1 : Recipe_List := Recipe_CSV.ReadCSV("test1.csv");
     List2 : Recipe_List := Recipe_CSV.ReadCSV("test2.csv");
     List3 : Recipe_List := Recipe_CSV.ReadCSV("test3.csv");
+    List4 : Recipe_List := Recipe_CSV.ReadCSV("test4.csv");
+    List5 : Recipe_List := Recipe_CSV.ReadCSV("donotexist.csv");
     procedure Print_Recipe(Recipe : Available_Recipe) is
         Cocktail : Recipes.Recipe := Recipe.Cocktail;
     begin
@@ -23,5 +25,11 @@ begin
     end loop;
     for I in 0 .. List3.Length - 1 loop
         Print_Recipe(List3.Get(I));
+    end loop;
+    for I in 0 .. List4.Length - 1 loop
+        Print_Recipe(List4.Get(I));
+    end loop;
+    for I in 0 .. List5.Length - 1 loop
+        Print_Recipe(List5.Get(I));
     end loop;
 end Test_CSV;
