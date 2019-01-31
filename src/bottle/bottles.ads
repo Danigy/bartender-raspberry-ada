5024b8d532bb3735ab5c3bb25294f898cfaa132b
Corentin LE BIGOT <lebigo_c@epita.fr>
2019-01-31 20:10:33 +0100

    bottle: Add contract
    Contracts are not commented anymore.
    
    Signed-off-by: Corentin LE BIGOT <lebigo_c@epita.fr>

--- src/bottle/bottles.ads
+++ src/bottle/bottles.ads
@@ -7,10 +7,10 @@ package Bottles is
         Name : String_Access;
         Remaining_Vol : Volume; -- Remaining Volume in the bottle in ml
     end record;
-    -- with Dynamic_Predicate => This.Remaining_Vol > 0;
+       with Dynamic_Predicate => This.Remaining_Vol > 0;
 
     procedure RemoveRemainingVolume (bot: in out Bottle; Vol : Positive);
-    --with Pre => bot.Remaining_Vol > Vol,
-    --     Post => bot.Remaining_vol >= 0 and then bot'old.Remaining_Vol - Vol > 0;
+        with Pre => bot.Remaining_Vol > Vol,
+             Post => bot.Remaining_vol >= 0 and then bot'old.Remaining_Vol - Vol > 0;
 
 end Bottles;
